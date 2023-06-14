9834e6e171856f72985e1aaf6e6ab0bee342f8e8
Jacob Marshall <me@jhqcat.com>
2023-06-14 10:16:13 +0100

    🚧 Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import words from "./data/words"
 
-// const unknownPos = []
-const knownPos = ["h", "o", "b", "", ""]
+const yellow = ["s", "", "", "", ""]
+const green = ["h", "o", "b", "", ""]
+const allowedLetters = []
+allowedLetters.concat(yellow)
+allowedLetters.concat(green)
 
 function arrayEquals(a: String[], b: String[]) {
   return Array.isArray(a) &&
@@ -14,10 +17,10 @@ function checker(value: string) {
   let accepted = ["", "", "", "", ""];
 
   for (let i = 0; i < value.length; i++) {
-    if (value.charAt(i) === knownPos[i]) accepted.splice(i, 1, value.charAt(i))
+    if (value.charAt(i) === green[i] && allowedLetters.includes(value.charAt(i))) accepted.splice(i, 1, value.charAt(i))
   }
 
-  if (arrayEquals(accepted, knownPos)) return true
+  if (arrayEquals(accepted, green)) return true
 }
 
 const newWords = words.filter(checker)
