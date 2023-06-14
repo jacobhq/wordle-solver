7acf3579b09f3d8813ef8e58550c6e7d01cf70a9
Jacob Marshall <me@jhqcat.com>
2023-06-14 09:54:53 +0100

    🚧 Known position works

--- index.ts
+++ index.ts
@@ -1,32 +1,24 @@
 import words from "./data/words"
 
-const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-
-let matchingWords = []
-
 // const unknownPos = []
-const knownPos = ["h", "", "", "", "", "e"]
+const knownPos = ["h", "o", "b", "", ""]
 
-// const check = "acids"
-
-function arrayEquals(a, b) {
+function arrayEquals(a: String[], b: String[]) {
   return Array.isArray(a) &&
     Array.isArray(b) &&
     a.length === b.length &&
-    a.every((val, index) => val === b[index]);
+    a.every((val, index) => val === b[index] || "");
 }
 
 function checker(value: string) {
-  let accepted = ["", "", "", "", "", ""];
-  
+  let accepted = ["", "", "", "", ""];
+
   for (let i = 0; i < value.length; i++) {
     if (value.charAt(i) === knownPos[i]) accepted.splice(i, 1, value.charAt(i))
   }
 
-  console.log(arrayEquals(accepted, knownPos), accepted)
   if (arrayEquals(accepted, knownPos)) return true
 }
 
 const newWords = words.filter(checker)
 console.log(newWords)
-// console.log(words.includes(check.toLowerCase()))
