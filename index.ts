ab3f467758ea50fa9052d59703c022fac6b99f44
Jacob Marshall <me@jhqcat.com>
2023-06-20 22:37:48 +0000

    🚀 Basic features work

--- index.ts
+++ index.ts
@@ -3,32 +3,32 @@ import * as data from "./data/letterProbability.json";
 
 const yellow = ["s", "e", "", "", ""];
 const green = ["h", "o", "", "", ""];
-const allowedLetters: String[] = [...yellow, ...green];
-console.log(allowedLetters);
-
-function arrayEquals(a: String[], b: String[]) {
-  return (
-    Array.isArray(a) &&
-    Array.isArray(b) &&
-    a.length === b.length &&
-    a.every((val, index) => val === b[index] || "")
-  );
-}
+const allowedLetters: string[] = [...yellow, ...green].filter((i) => i);
+const allowedEmptySlots = 5 - allowedLetters.length;
+console.log(allowedEmptySlots);
 
 function checker(value: string) {
-  let accepted = ["", "", "", "", ""];
-
-  for (let i = 0; i < value.length; i++) {
-    if (
-      value.charAt(i) === green[i] &&
-      allowedLetters.includes(value.charAt(i))
-    )
-      accepted.splice(i, 1, value.charAt(i));
+  // Check the word includes all the allowed letters
+  for (let i = 0; i < allowedLetters.length; i++) {
+    if (!value.includes(allowedLetters[i])) {
+      return false;
+    }
   }
 
-  if (arrayEquals(accepted, green)) return true;
+  // Check if green letters are in correct places
+  for (let i = 0; i < green.length; i++) {
+    const position = i;
+    if (green[i] !== "") {
+      if (value[position] !== green[i]) {
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
 
 const newWords = words.filter(checker);
 console.log(newWords);
-console.log(data.a);
+
+// TODO: Order array by letter probability, check against prev solutions (if yes, then cant happen again)
